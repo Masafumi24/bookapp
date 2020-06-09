7682006e8cbe8010928a35eba423ff78bec158ae
Masafumi24 <61171572+Masafumi24@users.noreply.github.com>
2020-06-09 21:04:50 +0900

    laravel class error

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+// use Illuminate\Support\Facades\Route;
+use APP\Book;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +16,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $books = Book::all();
+    return view('books', ['books' => $books]);
 });
